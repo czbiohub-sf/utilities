5f721669d0c02d7990e211a7fcde937555562f94
James Webber <james.webber@czbiohub.org>
2017-11-02 20:14:06 -0700

    Fixed bugs in aegea_launcher.py and modified it to make a bit easier. Moved aws_access.sh to the root directory because it's a common use. Added a new version of run_star_and_htseq.py that uses multiprocessing to (maybe) run more efficiently.

--- aegea_launcher.py
+++ aegea_launcher.py
@@ -6,6 +6,7 @@ import os
 import subprocess
 
 import boto3
+import botocore.exceptions
 
 
 def resource_range(name, min_val, max_val):
@@ -19,7 +20,7 @@ def resource_range(name, min_val, max_val):
             raise argparse.ArgumentTypeError(msg)
         return value
 
-    return resource_range
+    return range_validator
 
 
 def get_logger(debug, dryrun):
@@ -51,53 +52,69 @@ if __name__ == '__main__':
                 "e.g. aegea_launcher.py my_bucket/my_scripts "
                 "[script name] [script args...]"
             ),
-            epilog="See https://github.com/czbiohub/utilities for more examples"
+            epilog="See https://github.com/czbiohub/utilities for more examples",
+            add_help=False
     )
 
     # basic usage
-    basic_group = parser.add_argument_group('basic')
-    basic_group.add_argument('s3_script_path',
-                             help=('S3 bucket/path to scripts.\n'
-                                   'e.g. jamestwebber-logs/scripts'))
-    basic_group.add_argument('script_name',
-                             help='Name of the script to run')
-    basic_group.add_argument('script_args', nargs='*',
-                             help='Script arguments. Wildcards not recommended')
+    basic_group = parser.add_argument_group('basic arguments')
+    basic_group.add_argument(
+            's3_script_path',
+            help='S3 bucket/path to scripts. e.g. jamestwebber-logs/scripts'
+    )
+    basic_group.add_argument(
+            'script_name',
+            help='Name of the script to run, e.g. bcl2fastq.py'
+    )
+    basic_group.add_argument('script_args',
+            help='Script arguments, as a string. e.g. "--taxon mus".'
+    )
 
 
     # instance requirements
-    instance_group = parser.add_argument_group('instance')
-    instance_group.add_argument('--ecr-image', default='sra_download',
-                                help='ECR image to use for the job')
-    # instance_group.add_argument('--ami', default=None,
-    #                             help='AMI to use for the job')
+    instance_group = parser.add_argument_group('customize the instance')
+    image_group = instance_group.add_mutually_exclusive_group()
+    image_group.add_argument('--ecr-image', metavar='ECR',
+                             help='ECR image to use for the job')
+    image_group.add_argument('--ami',
+                             help='AMI to use for the job')
 
     instance_group.add_argument('--queue', default='aegea_batch',
-                           help='Queue to submit the job')
+                                help='Queue to submit the job')
     instance_group.add_argument('--vcpus', default=1,
-                           type=resource_range('vcpus', 1, 64),
-                           help='Number of vCPUs needed, e.g. 16')
-    instance_group.add_argument('--memory', default=4000,
-                           type=resource_range('memory', 0, 256000),
-                           help='Amount of memory needed, in MB, e.g. 16000')
-    instance_group.add_argument('--storage', default=None,
-                           type=resource_range('storage', 500, 16000),
-                           help='Request additional storage, in GiB (min 500)')
-    instance_group.add_argument('--ulimits', default=None, nargs='+',
-                           help='Change instance ulimits, e.g. nofile:1000000')
-    instance_group.add_argument('--environment', default=None, nargs='+',
-                           help='Set environment variables')
+                                type=resource_range('vcpus', 1, 64),
+                                help='Number of vCPUs needed, e.g. 16')
+    instance_group.add_argument(
+            '--memory', default=4000, type=resource_range('memory', 0, 256000),
+            help='Amount of memory needed, in MB, e.g. 16000'
+    )
+    instance_group.add_argument(
+            '--storage', default=None,
+            type=resource_range('storage', 500, 16000),
+            help='Request additional storage, in GiB (min 500)'
+    )
+    instance_group.add_argument(
+            '--ulimits', metavar='U', default=None, nargs='+',
+            help='Change instance ulimits, e.g. nofile:1000000'
+    )
+    instance_group.add_argument('--environment', metavar='ENV', default=None,
+                                nargs='+', help='Set environment variables')
 
     # other arguments
-    other_group = parser.add_argument_group('other')
+    other_group = parser.add_argument_group('other options')
     other_group.add_argument('--dryrun', action='store_true',
-                             help="Print the job command but don't launch it")
-    other_group.add_argument('--upload', action='store_true',
+                             help="Print the command but don't launch the job")
+    other_group.add_argument('-u', '--upload', action='store_true',
                              help="Upload the script to S3 before running")
-    other_group.add_argument('--debug', action='store_true',
+    other_group.add_argument('-d', '--debug', action='store_true',
                              help="Set logging to debug level")
+    other_group.add_argument('-h', '--help', action='help',
+                             help="show this help message and exit")
 
     args = parser.parse_args()
+
+    if not args.ecr_image or args.ami:
+        args.ecr_image = 'sra_download'
 
     logger = get_logger(args.debug, args.dryrun)
 
@@ -107,12 +124,12 @@ if __name__ == '__main__':
     s3_key = os.path.join(os.sep.join(os.path.split(args.s3_script_path)[1:]),
                           script_base)
 
+    logger.debug("Starting S3 client")
+    client = boto3.client('s3')
+
     if args.upload:
         if not os.path.exists(args.script_name):
             raise ValueError("Can't find script: {}".format(args.script_name))
-
-        logger.debug("Starting S3 client")
-        client = boto3.client('s3')
 
         logger.info("Uploading {} to s3://{}".format(
                 args.script_name, os.path.join(s3_bucket, s3_key))
@@ -126,17 +143,30 @@ if __name__ == '__main__':
                     Bucket=s3_bucket,
                     Key=s3_key
             )
+    else:
+        try:
+            client.head_object(
+                    Bucket=s3_bucket,
+                    Key=s3_key
+            )
+        except botocore.exceptions.ClientError:
+            raise ValueError("{} is not on s3, you should upload it.".format(
+                    args.script_name))
 
     job_command = "aws s3 cp s3://{} .; chmod 755 {}; ./{} {}".format(
             os.path.join(s3_bucket, s3_key),
-            script_base, script_base, ' '.join(args.script_args)
+            script_base, script_base, args.script_args
     )
 
     aegea_command = ['aegea', 'batch', 'submit',
                      '--queue', args.queue,
                      '--vcpus', str(args.vcpus),
-                     '--memory', str(args.memory),
-                     '--ecr-image', args.ecr_image]
+                     '--memory', str(args.memory)]
+
+    if args.ecr_image:
+        aegea_command.extend(['--ecr-image', args.ecr_image])
+    elif args.ami:
+        aegea_command.extend(['--ami', args.ami])
 
     if args.storage:
         aegea_command.extend(['--storage', '/mnt={}'.format(args.storage)])
@@ -151,5 +181,5 @@ if __name__ == '__main__':
 
     logger.info('executing command:\n\t{}'.format(' '.join(aegea_command)))
     if not args.dryrun:
-        output = subprocess.check_output(aegea_command, shell=True)
-        logger.debug(output)
+        output = subprocess.check_output(' '.join(aegea_command), shell=True)
+        logger.info(output)
---
+++ run_star_and_htseq.py
@@ -0,0 +1,416 @@
+#!/usr/bin/env python
+import argparse
+import subprocess
+import os
+import thread
+import datetime
+
+import re
+
+import threading
+import multiprocessing as mp
+
+import logging
+
+S3_RETRY = 5
+S3_LOG_DIR = 's3://jamestwebber-logs/star_logs/'
+
+STAR = "/usr/local/bin/STAR"
+HTSEQ = "htseq-count"
+SAMTOOLS = "samtools"
+
+COMMON_PARS = [STAR,
+               '--outFilterType', 'BySJout',
+               '--outFilterMultimapNmax', '20',
+               '--alignSJoverhangMin', '8',
+               '--alignSJDBoverhangMin', '1',
+               '--outFilterMismatchNmax', '999',
+               '--outFilterMismatchNoverLmax', '0.04',
+               '--alignIntronMin', '20',
+               '--alignIntronMax', '1000000',
+               '--alignMatesGapMax', '1000000',
+               '--outSAMstrandField', 'intronMotif',
+               '--outSAMtype', 'BAM', 'Unsorted',
+               '--outSAMattributes', 'NH', 'HI', 'NM', 'MD',
+               '--genomeLoad', 'LoadAndKeep',
+               '--outReadsUnmapped', 'Fastx',
+               '--readFilesCommand', 'zcat']
+
+CURR_MIN_VER = datetime.date(2017, 3, 1)
+
+
+def process_logs(q, logger):
+    for msg,level in iter(q.get, 'STOP'):
+        if level == logging.INFO:
+            logger.info(msg)
+        else:
+            logger.debug(msg)
+
+
+def log_command(logger, command, **kwargs):
+    logger.info(' '.join(command))
+    output = subprocess.check_output(' '.join(command), **kwargs)
+    logger.debug(output)
+
+
+def log_command_to_queue(log_queue, command, **kwargs):
+    log_queue.put(' '.join(command), logging.INFO)
+    output = subprocess.check_output(' '.join(command), **kwargs)
+    log_queue.put(output, logging.DEBUG)
+
+
+# def run_sample(sample_name, exp_id):
+def run_sample(star_queue, htseq_queue, log_queue, genome_dir, n_proc):
+    for sample_name,exp_id in iter(star_queue.get, 'STOP'):
+        log_queue.put('{} - {}'.format(exp_id, sample_name), logging.INFO)
+        dest_dir = os.path.join(DEST_DIR, sample_name)
+        os.makedirs(dest_dir)
+        os.mkdir(os.path.join(dest_dir, 'rawdata'))
+        os.mkdir(os.path.join(dest_dir, 'results'))
+
+        # copy fastq.gz from s3 to local
+        s3_source = os.path.join(s3_input_dir, exp_id, 'rawdata', sample_name)
+        command = ['aws', 's3', 'cp', '--recursive',
+                   s3_source, os.path.join(dest_dir, 'rawdata'),
+                   '--exclude', "'*'",
+                   '--include', "'*.fastq.gz'"]
+        for i in range(S3_RETRY):
+            try:
+                log_command_to_queue(log_queue, command, shell=True)
+                break
+            except subprocess.CalledProcessError:
+                log_queue.put("retrying data download - {}".format(i),
+                              logging.DEBUG)
+        else:
+            raise RuntimeError("couldn't download fastq.gz files")
+
+        # start running STAR
+        # getting input files first
+
+        reads = glob.glob(os.path.join(dest_dir, 'rawdata', '*.fastq.gz'))
+        if not reads:
+            log_queue.put("Empty reads for %s" % s3_source, logging.INFO)
+            return
+
+        os.makedir(os.path.join(dest_dir, 'results', 'Pass1'))
+
+        command = COMMON_PARS[:]
+        command.extend(('--runThreadN', str(n_proc),
+                        '--genomeDir', genome_dir,
+                        '--readFilesIn', ' '.join(reads)))
+        log_command_to_queue(log_queue, command, shell=True,
+                             cwd=os.path.join(dest_dir, 'results', 'Pass1'))
+
+        # running sam tools
+        command = [SAMTOOLS, 'sort', '-m', '6000000000', '-o',
+                   './Pass1/Aligned.out.sorted.bam', './Pass1/Aligned.out.bam']
+        log_command_to_queue(log_queue, command, shell=True,
+                    cwd=os.path.join(dest_dir, 'results'))
+
+        # running samtools index -b
+        command = [SAMTOOLS, 'index', '-b', 'Aligned.out.sorted.bam']
+        log_command_to_queue(log_queue, command, shell=True,
+                             cwd=os.path.join(dest_dir, 'results', 'Pass1'))
+
+
+        # remove unsorted bam files
+        os.remove(os.path.join(dest_dir, 'results', 'Pass1', 'Aligned.out.bam'))
+
+        # remove fastq files
+        for fastq_file in reads:
+            os.remove(fastq_file)
+
+        # generating files for htseq-count
+        command = [SAMTOOLS, 'sort', '-m', '6000000000', '-n', '-o',
+                   './Pass1/Aligned.out.sorted-byname.bam',
+                   './Pass1/Aligned.out.sorted.bam']
+        log_command_to_queue(log_queue, command, shell=True,
+                             cwd=os.path.join(dest_dir, 'results'))
+
+        # ready to be htseq-ed and cleaned up
+        htseq_queue.put((exp_id, sample_name, dest_dir))
+
+
+def run_htseq(htseq_queue, log_queue, taxon, sjdb_gtf):
+    for exp_id, sample_name, dest_dir in iter(htseq_queue.get, 'STOP'):
+        # running htseq
+
+        command = [HTSEQ,
+                   '-r', 'name', '-s', 'no', '-f', 'bam',
+                   '-m', '-intersection-nonempty',
+                   os.path.join(dest_dir, 'results', 'Pass1',
+                                'Aligned.out.sorted-byname.bam'),
+                   sjdb_gtf, '>', 'htseq-count.txt']
+        log_command_to_queue(log_queue, command, shell=True,
+                             cwd=os.path.join(dest_dir, 'results'))
+        os.remove(os.path.join(dest_dir, 'results', 'Pass1',
+                               'Aligned.out.sorted-byname.bam'))
+
+
+        # compressed the results dir and move it to s3
+        command = ['tar', '-cvfz',
+                   '{}.{}.tgz',format(sample_name, taxon),
+                   'results']
+        log_command_to_queue(log_queue, command, shell=True, cwd=dest_dir)
+
+        # copy htseq and log files out to s3
+        s3_dest = os.path.join(s3_input_dir, exp_id, 'results/')
+
+        src_files = [
+            os.path.join(dest_dir, '{}.{}.tgz'.format(sample_name, taxon)),
+            os.path.join(dest_dir, 'results', 'htseq-count.txt'),
+            os.path.join(dest_dir, 'results', 'Pass1', 'Log.final.out')
+        ]
+
+        dest_names = [
+            '{}.{}.tgz'.format(sample_name, taxon),
+            '{}.{}.htseq-count.txt'.format(sample_name, taxon),
+            '{}.{}.log.final.out'.format(sample_name, taxon)
+        ]
+
+        for src_file,dest_name in zip(src_files, dest_names):
+            command = ['aws', 's3', 'cp', file_name,
+                       os.path.join(s3_dest, dest_name)]
+
+            log_command_to_queue(log_queue, command, shell=True)
+
+        # rm all the files
+        command = ['rm', '-rf', dest_dir]
+        log_command_to_queue(log_queue, command, shell=True)
+
+
+def main(logger):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--root_dir', default='/mnt')
+    parser.add_argument('--dest_dir', default='/mnt/data/hca/')
+    parser.add_argument('--taxon', default='homo', choices=('homo', 'mus'))
+
+    parser.add_argument('--s3_input_dir')
+    parser.add_argument('--num_partitions', type=int)
+    parser.add_argument('--partition_id', type=int)
+    parser.add_argument('--exp_ids', nargs='+')
+
+    parser.add_argument('--star_proc', type=int, default=8,
+                        help='Number of processes to give to each STAR run')
+    parser.add_argument('--htseq_proc', type=int, default=4,
+                        help='Number of htseq processes to run')
+
+    args = parser.parse_args()
+
+    if os.environ.get('AWS_BATCH_JOB_ID'):
+        args.root_dir = os.path.join(args.root_dir, os.environ['AWS_BATCH_JOB_ID'])
+        args.dest_dir = os.path.join(args.root_dir, 'data' 'hca')
+
+    if args.taxon == 'homo':
+        genome_dir = os.path.join(args.root_dir, "genome/STAR/HG38-PLUS/") # change
+        ref_genome_file = 'hg38-plus.tgz'
+        ref_genome_star_file = 'HG38-PLUS.tgz'
+        sjdb_gtf = os.path.join(args.root_dir, 'genome', 'hg38-plus',
+                                'hg38-plus.gtf')
+    elif args.taxon == 'mus':
+        genome_dir = os.path.join(args.root_dir, "genome/STAR/MM10-PLUS/")
+        ref_genome_file = 'mm10-plus.tgz'
+        ref_genome_star_file = 'MM10-PLUS.tgz'
+        sjdb_gtf = os.path.join(args.root_dir, 'genome', 'mm10-plus',
+                                'mm10-plus.gtf')
+
+    else:
+        raise ValueError('Invalid taxon {}'.format(args.taxon))
+
+    if args.star_proc > mp.cpu_count():
+        raise ValueError('Not enough CPUs to give {} processes to STAR'.format(
+                args.star_proc))
+
+
+    logger.info(
+            '''Run Info: partition {} out of {}
+                   star_proc:\t{}
+                  htseq_proc:\t{}
+                  genome_dir:\t{}
+             ref_genome_file:\t{}
+        ref_genome_star_file:\t{}
+                    sjdb_gtf:\t{}
+                       taxon:\t{}
+                s3_input_dir:\t{}
+                     exp_ids:\t{}'''.format(
+                    args.star_proc, args.htseq_proc,
+                    args.partition_id, args.num_partitions,
+                    genome_dir, ref_genome_file,
+                    ref_genome_star_file, sjdb_gtf,
+                    args.taxon, args.s3_input_dir,
+                    ', '.join(args.exp_ids)
+            )
+    )
+
+    # download the genome data
+    os.makedirs(os.path.join(args.root_dir, 'genome'))
+    command = ['aws', 's3', 'cp',
+               os.path.join('s3://czi-hca', 'ref-genome', ref_genome_file),
+               os.path.join(args.root_dir, 'genome')]
+    log_command(logger, command, shell=True)
+
+    logger.debug('Extracting {}'. format(ref_genome_file))
+    with tarfile.open(os.path.join(args.root_dir, 'genome',
+                                   ref_genome_file)) as tf:
+        tf.extractall(path=os.path.join(args.root_dir ,'genome'))
+
+
+
+    # download STAR stuff
+    os.makedir(os.path.join(args.root_dir, 'genome', 'STAR'))
+    command = ['aws', 's3', 'cp',
+               os.path.join('s3://czi-hca', 'ref-genome', 'STAR',
+                            ref_genome_star_file),
+               os.path.join(args.root_dir, 'genome', 'STAR')]
+    log_command(logger, command, shell=True)
+
+    logger.debug('Extracting {}'.format(ref_genome_star_file))
+    with tarfile.open(os.path.join(args.root_dir, 'genome',
+                                   'STAR', ref_genome_star_file)) as tf:
+        tf.extractall(path=os.path.join(args.root_dir ,'genome', 'STAR'))
+
+
+    # Load Genome Into Memory
+    command = [STAR, '--genomeDir', genome_dir, '--genomeLoad', 'LoadAndExit']
+    log_command(logger, command, shell=True)
+
+    log_queue = mp.Queue()
+    log_thread = threading.Thread(target=logger_thread,
+                                  args=(log_queue, logger))
+    log_thread.start()
+
+    star_queue = mp.Queue()
+    htseq_queue = mp.Queue()
+
+    n_star_procs = mp.cpu_count() / args.star_proc
+
+    star_procs = [mp.Process(target=run_sample,
+                             args=(star_queue, htseq_queue, log_queue,
+                                   genome_dir, args.star_proc))
+                  for i in range(n_star_procs)]
+
+    for p in star_procs:
+        p.start()
+
+    htseq_procs = [mp.Process(target=run_htseq,
+                              args=(htseq_queue, log_queue, taxon, sjdb_gtf))
+                   for i in range(args.htseq_proc)]
+
+    for p in htseq_procs:
+        p.start()
+
+
+    for exp_id in exp_ids:
+        if exp_id.startswith('Undetermined'):
+            logger.info("Skipping file: %s" % exp_id)
+            continue
+
+        # Check the exp_id folder for existing runs
+        try:
+            command = ['aws', 's3', 'ls',
+                       os.path.join(args.s3_input_dir, exp_id, 'results/')]
+            logger.info(' '.join(command))
+            output = subprocess.check_output(' '.join(command),
+                                             shell=True).split('\n')
+        except subprocess.CalledProcessError:
+            logger.info("Nothing in the results directory")
+            output = []
+
+        output_files = {(tuple(line[:10].split('-')), line.split()[-1])
+                        for line in output
+                        if line.strip().endswith('htseq-count.txt')}
+        output_files = {fn for dt,fn in output_files
+                        if datetime.date(*map(int, dt)) > CURR_MIN_VER}
+        logger.info("number of files: {}".format(len(output_files)))
+
+        logger.info("Running partition {} of {} for exp {}".format(
+                partition_id, num_partitions, exp_id)
+        )
+
+        command = ['aws', 's3', 'ls',
+                   os.path.join(args.s3_input_dir, exp_id, 'rawdata/')]
+        logger.info(' '.join(command))
+        try:
+            output = subprocess.check_output(' '.join(command),
+                                             shell=True).split("\n")
+        except subprocess.CalledProcessError:
+            logger.info("error in aws command", exc_info=True)
+            output = []
+
+        sample_list = []
+
+        for f in output:
+            matched = re.search("\s([\d\w\-.]+)/", f)
+            if matched:
+                sample_list.append(matched.group(1))
+
+        for sample_name in sample_list[partition_id::num_partitions]:
+            if '{}.{}.htseq-count.txt'.format(sample_name, TAXON) in output_files:
+                logger.info("{} already exists, skipping".format(sample_name))
+                continue
+
+            logger.info("Adding sample {} to queue".format(sample_name))
+            star_queue.put((sample_name, exp_id))
+
+
+    for i in range(n_star_procs):
+        star_queue.put('STOP')
+
+    for p in star_procs:
+        p.join()
+
+    for i in range(args.htseq_proc):
+        htseq_queue.put('STOP')
+
+    for p in htseq_procs:
+        p.join()
+
+    log_queue.put('STOP')
+    log_thread.join()
+
+    # Remove Genome from Memory
+    command = [STAR, '--genomeDir', genome_dir, '--genomeLoad', 'Remove']
+    log_command(logger, command, shell=True)
+
+    logger.info('Job completed')
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    mainlogger = logging.getLogger(__name__)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+
+    # create a logging format
+    formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    stream_handler.setFormatter(formatter)
+
+    mainlogger.addHandler(stream_handler)
+
+    if os.environ.get('AWS_BATCH_JOB_ID'):
+        log_file = os.path.join(
+                ROOT_DIR, '{}.log'.format(os.environ['AWS_BATCH_JOB_ID'])
+        )
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(formatter)
+
+        # add the handlers to the logger
+        mainlogger.addHandler(file_handler)
+    else:
+        log_file = None
+
+    try:
+        main(mainlogger)
+    finally:
+        if log_file:
+            log_cmd = 'aws s3 cp {} {}'.format(log_file, S3_LOG_DIR)
+            mainlogger.info(log_cmd)
+
+            handler.close()
+            subprocess.check_output(log_cmd, shell=True)
