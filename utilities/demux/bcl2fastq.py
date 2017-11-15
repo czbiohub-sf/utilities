0a0e766ad5a0a8c026564b852fc53a37cec967bb
James Webber <james.webber@czbiohub.org>
2017-11-15 14:03:10 -0800

    Changed the default queue for demux jobs

--- utilities/demux/bcl2fastq.py
+++ utilities/demux/bcl2fastq.py
@@ -17,6 +17,7 @@ S3_LOG_DIR = 's3://jamestwebber-logs/bcl2fastq_logs/'
 
 def get_default_requirements():
     return argparse.Namespace(vcpus=64, memory=256000, storage=1200,
+                              queue='aegea_batch_demux',
                               ulimits=['nofile:1000000'])
 
 
