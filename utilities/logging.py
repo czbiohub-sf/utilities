d63d00b22e280e990b0d080314483efc2d3e9ef5
James Webber <james.webber@czbiohub.org>
2018-07-13 18:28:32 -0700

    Silly bug from naming my module after the built-in module that it imports...

--- utilities/logging.py
+++ utilities/logging.py
@@ -1,4 +1,4 @@
-import logging
+import logging as _logging
 import os
 import subprocess
 import threading
@@ -13,7 +13,7 @@ def log_command(logger, command, **kwargs):
 
 
 def log_command_to_queue(log_queue, command, **kwargs):
-    log_queue.put((' '.join(command), logging.INFO))
+    log_queue.put((' '.join(command), _logging.INFO))
     try:
         output = subprocess.check_output(' '.join(command), **kwargs)
         failed = False
@@ -21,34 +21,34 @@ def log_command_to_queue(log_queue, command, **kwargs):
         output = "Command failed!"
         failed = True
 
-    log_queue.put((output, logging.DEBUG))
+    log_queue.put((output, _logging.DEBUG))
     return failed
 
 
 def process_logs(q, logger):
     for msg,level in iter(q.get, 'STOP'):
-        if level == logging.INFO:
+        if level == _logging.INFO:
             logger.info(msg)
         else:
             logger.debug(msg)
 
 
 def get_logger(name, debug=False, dryrun=False):
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
+    logger = _logging.getLogger(name)
+    logger.setLevel(_logging.DEBUG)
 
     # create a logging format
     if dryrun:
-        formatter = logging.Formatter(
+        formatter = _logging.Formatter(
                 '%(asctime)s - %(name)s - %(levelname)s - (DRYRUN) - %(message)s'
         )
     else:
-        formatter = logging.Formatter(
+        formatter = _logging.Formatter(
                 '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         )
 
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG if debug else logging.INFO)
+    stream_handler = _logging.StreamHandler()
+    stream_handler.setLevel(_logging.DEBUG if debug else _logging.INFO)
     stream_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
@@ -57,8 +57,8 @@ def get_logger(name, debug=False, dryrun=False):
         log_file = os.path.abspath(
                 '{}.log'.format(os.environ['AWS_BATCH_JOB_ID'])
         )
-        file_handler = logging.FileHandler(log_file)
-        file_handler.setLevel(logging.DEBUG)
+        file_handler = _logging.FileHandler(log_file)
+        file_handler.setLevel(_logging.DEBUG)
         file_handler.setFormatter(formatter)
 
         # add the handlers to the logger
