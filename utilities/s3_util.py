73ae9314e0688ec45176a8d9368366e63f4cd1b0
James Webber <james.webber@czbiohub.org>
2018-08-08 12:49:47 -0700

    Changing this back to the way it was before–creating boto3 objects in thread initializers was not working at all.

--- utilities/s3_util.py
+++ utilities/s3_util.py
@@ -1,5 +1,6 @@
+import os
+
 import boto3
-import botocore.exceptions
 
 from boto3.s3.transfer import TransferConfig
 
@@ -12,28 +13,6 @@ def s3_bucket_and_key(s3_uri):
     assert s3_uri.startswith(prefix)
 
     return s3_uri[len(prefix):].split("/", 1)
-
-
-# function to init a pool process with an s3 client and bucket
-def init_client(b, nb=None):
-    global s3c
-    s3c = boto3.client('s3')
-
-    global bucket
-    bucket = b
-
-    if nb is not None:
-        global new_bucket
-        new_bucket = nb
-
-
-# function to init a pool process with an s3 resource and bucket
-def init_resource(b):
-    global s3r
-    s3r = boto3.resource('s3')
-
-    global bucket
-    bucket = b
 
 
 def prefix_gen(bucket, prefix, fn=None):
@@ -62,67 +41,81 @@ def get_size(bucket='czbiohub-seqbot', prefix=None):
 
 def get_status(file_list, bucket_name='czbiohub-seqbot'):
     """Print the storage/restore status for a list of keys"""
-    s3r = boto3.resource('s3')
+    s3res = boto3.resource('s3')
 
     for fn in file_list:
-        obj = s3r.Object(bucket_name, fn)
+        obj = s3res.Object(bucket_name, fn)
         print(obj.key, obj.storage_class, obj.restore)
 
 
 def restore_file(k):
     obj = s3r.Object('czbiohub-seqbot', k)
-    if not obj.restore:
+    if obj.storage_class == 'GLACIER' and not obj.restore:
         bucket.meta.client.restore_object(
             Bucket='czbiohub-seqbot',
             Key=k,
-            RestoreRequest={'Days': 3}
+            RestoreRequest={'Days': 7}
         )
 
 
-def restore_files(file_list, *, b='czbiohub-seqbot', n_proc=16):
+def restore_files(file_list, *, n_proc=16):
     """Restore a list of files from czbiohub-seqbot in parallel"""
 
+    global s3r
+    s3r = boto3.resource('s3')
+    global bucket
+    bucket = s3r.Bucket('czbiohub-seqbot')
+
     print('creating pool')
-    p = multiprocessing.Pool(processes=n_proc,
-                             initializer=init_resource,
-                             initargs=(b,))
+    p = multiprocessing.Pool(processes=n_proc)
 
-    try:
-        print('restoring files...')
-        p.map(restore_file, file_list, chunksize=100)
-    finally:
-        p.close()
-        p.join()
+    print('restoring files...')
+    p.map(restore_file, file_list, chunksize=64)
+
+    p.close()
+    p.join()
 
 
-def copy_file(k):
-    key, new_key = k
-    try:
-        s3c.head_object(Bucket=new_bucket, Key=new_key)
-    except botocore.exceptions.ClientError:
-        s3c.copy(CopySource={'Bucket': bucket, 'Key': key},
-                 Bucket=new_bucket,
-                 Key=new_key)
-                 # Config=TransferConfig(use_threads=False))
+def copy_file(key, new_key):
+    s3c.copy(CopySource={'Bucket': bucket, 'Key': key},
+             Bucket=new_bucket,
+             Key=new_key,
+             Config=TransferConfig(use_threads=False))
 
 
-def copy_files(src_list, dest_list, *, b, nb, n_proc=16):
+def copy_files(src_list, dest_list, *, b, nb, force_copy=False, n_proc=16):
     """
     Copy a list of files from src_list to dest_list.
     b - original bucket
     nb - destination bucket
     """
 
-    p = multiprocessing.Pool(processes=n_proc,
-                             initializer=init_client,
-                             initargs=(b, nb))
+    global s3c
+    s3c = boto3.client('s3')
 
-    try:
-        print('copying files')
-        p.map(copy_file, zip(src_list, dest_list), chunksize=100)
-    finally:
-        p.close()
-        p.join()
+    global bucket
+    bucket = b
+    global new_bucket
+    new_bucket = nb
+
+    if not force_copy:
+        existing_files = set(get_files(
+            bucket=nb, prefix=os.path.commonprefix(dest_list)
+        )) & set(dest_list)
+    else:
+        existing_files = set()
+
+    print('creating pool')
+    p = multiprocessing.Pool(processes=n_proc)
+
+    print('copying files')
+    p.starmap(copy_file,
+              ((src,dest) for src,dest in zip(src_list, dest_list)
+               if dest not in existing_files),
+              chunksize=64)
+
+    p.close()
+    p.join()
 
 
 def remove_file(k):
@@ -134,35 +127,50 @@ def remove_files(file_list, *, b, really=False, n_proc=16):
 
     assert really
 
-    p = multiprocessing.Pool(processes=n_proc,
-                             initializer=init_client,
-                             initargs=(b,))
+    global s3c
+    s3c = boto3.client('s3')
 
-    try:
-        print("Removing {} files!".format(len(file_list)))
-        p.map(remove_file, file_list, chunksize=100)
-    finally:
-        p.close()
-        p.join()
+    global bucket
+    bucket = b
+
+    print('creating pool')
+    p = multiprocessing.Pool(processes=n_proc)
+
+    print("Removing {} files!".format(len(file_list)))
+    p.map(remove_file, file_list, chunksize=64)
+
+    p.close()
+    p.join()
 
 
-def download_file(k):
-    key, dest = k
+def download_file(key, dest):
     s3c.download_file(Bucket=bucket,
                       Key=key,
                       Filename=dest,
                       Config=TransferConfig(use_threads=False))
 
 
-def download_files(src_list, dest_list, *, b, n_proc=16):
+def download_files(src_list, dest_list, *, b, force_download=False, n_proc=16):
     """Download a list of file to local storage"""
 
-    p = multiprocessing.Pool(processes=n_proc,
-                             initializer=init_client,
-                             initargs=(b,))
+    global s3c
+    s3c = boto3.client('s3')
 
-    try:
-        p.map(download_file, zip(src_list, dest_list), chunksize=100)
-    finally:
-        p.close()
-        p.join()
+    global bucket
+    bucket = b
+
+    if not force_download:
+        existing_files = set(fn for fn in dest_list if os.path.exists(fn))
+    else:
+        existing_files = set()
+
+    p = multiprocessing.Pool(processes=n_proc)
+
+    # downloading files
+    p.starmap(download_file,
+              ((src, dest) for src, dest in zip(src_list, dest_list)
+               if dest not in existing_files),
+              chunksize=64)
+
+    p.close()
+    p.join()
