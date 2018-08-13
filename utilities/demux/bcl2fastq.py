fae6f8270c80db4a90954838c5bbe53ca9c14b34
James Webber <james.webber@czbiohub.org>
2018-08-13 10:17:58 -0700

    Upped the default storage for bcl2fastq–it's mostly getting used for NovaSeq runs and we have auto-cleanup of unused volumes now.

--- utilities/demux/bcl2fastq.py
+++ utilities/demux/bcl2fastq.py
@@ -17,7 +17,7 @@ S3_LOG_DIR = 's3://jamestwebber-logs/bcl2fastq_logs/'
 
 
 def get_default_requirements():
-    return argparse.Namespace(vcpus=64, memory=256000, storage=2000,
+    return argparse.Namespace(vcpus=64, memory=256000, storage=5000,
                               queue='aegea_batch_demux',
                               ecr_image='demuxer',
                               ulimits=['nofile:1000000'])
