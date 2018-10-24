c0d4e2bdc6dd888ef0dc1e4a97fe48a6da024d92
James Webber <jamestwebber@users.noreply.github.com>
2018-10-24 14:19:40 -0700

    Big refactor (#24)
    * major refactor, switching to click and src structure
    * reformatted with black, and changed to entry_points, closes #23 
    * Changed s3_util to use concurrent.future and rewrote some functions to be a bit cleaner.
    * Added velocyto image and script
    * Made aws_upload permissions more relaxed
    * Updating config.yml files for the docker images, and made aegea an optional extra so I don't have to deal with its wack dependencies
    * Added `get_folders` function to return the list of folders (common prefixes) under a given s3 path.
    * added optional `queue` argument to get_thread_logger to let it use a managed queue
    * Added miniconda3 to demuxer so that utilities package installs correctly
    * Fixed bug in run_star_and_hsteq, taxon should be required

---
+++ src/utilities/alignment/combine_tables.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+
+import argparse
+import csv
+
+
+def combine_files(fileA, fileB, output_file):
+    with open(fileA) as fA, open(fileB) as fB:
+        rdrA = csv.DictReader(fA)
+        rdrB = csv.DictReader(fB)
+
+        hA = rdrA.fieldnames
+        hB = rdrB.fieldnames
+
+        hU = sorted(set(hA[1:]) | set(hB[1:]))
+
+        print(
+            "{} cells in file A, {} in file B, {} total".format(
+                len(hA), len(hB), len(hU)
+            )
+        )
+
+        with open(output_file, "w") as OUT:
+            print("gene,{}".format(",".join(hU)), file=OUT)
+
+            for i, (rA, rB) in enumerate(zip(rdrA, rdrB)):
+                if rA["gene"] != rB["gene"]:
+                    raise ValueError(
+                        "Gene list mismatch: {} != {}".format(rA["gene"], rB["gene"])
+                    )
+
+                summed_r = [rA["gene"]]
+                summed_r.extend(str(int(rA.get(c, 0)) + int(rB.get(c, 0))) for c in hU)
+                print(",".join(summed_r), file=OUT)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="combine_tables.py",
+        description=(
+            "Combine the gene-cell counts from two flow cells\n"
+            "e.g. ./combine_tables.py fileA fileB output_file\n"
+            "Input files should have genes as rows and cells as columns."
+        ),
+    )
+
+    parser.add_argument("fileA", help="Gene cell table for first flow cell")
+    parser.add_argument("fileB", help="Gene cell table for second flow cell")
+    parser.add_argument("output_file", help="File to write combined counts")
+
+    args = parser.parse_args()
+
+    combine_files(args.fileA, args.fileB, args.output_file)
