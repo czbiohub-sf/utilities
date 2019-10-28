e2bbd6e8b72dceeabf6f8069009d3aa49501765f
Shayan Hosseinzadeh <shayan.hosseinzadeh@czbiohub.org>
2019-10-28 10:58:06 -0700

    Fixed zebrafish naming convention

--- src/utilities/alignment/run_star_and_htseq.py
+++ src/utilities/alignment/run_star_and_htseq.py
@@ -26,7 +26,7 @@ reference_genomes = {
     "microcebus": "MicMur3-PLUS",
     "gencode.vM19": "gencode.vM19",
     "gencode.vM19.ERCC": "gencode.vM19.ERCC.SP1",
-    "zebrafish-plus": "danio_rerio_plus"
+    "zebrafish-plus": "danio_rerio_plus_STAR2.6.1d"
 }
 
 deprecated = {"homo": "hg38-plus", "mus": "mm10-plus"}
