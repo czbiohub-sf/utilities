0d08e8b904c020d73c879a34a08fd91d97b6db77
danledinh <ubuntu@ip-172-31-37-245.us-west-2.compute.internal>
2019-11-07 05:44:17 +0000

    added GRCh38_premrna to dictionary

--- src/utilities/alignment/10x_count.py
+++ src/utilities/alignment/10x_count.py
@@ -29,6 +29,7 @@ reference_genomes = {
     "hg19-mm10-3.0.0": "hg19-mm10-3.0.0",
     "microcebus": "MicMur3-PLUS",
     "gencode.vM19": "gencode.vM19",
+    "GRCh38_premrna":"GRCh38_premrna",
 }
 
 deprecated = {"homo", "mus", "mus-premrna"}
