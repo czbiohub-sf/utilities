9d59085d8177ae1c39308eaeae1a9a3bd080927d
Qiqing Li <qiiqng.li@berkeley.edu>
2020-06-24 22:46:14 -0700

    loompy debugging fixed matched variable referenced before assignment

--- src/utilities/velocyto/run_loompy.py
+++ src/utilities/velocyto/run_loompy.py
@@ -54,15 +54,17 @@ def get_parser():
         choices=list(reference_genomes_indexes.keys()),
         help="Reference genome index for the alignment and RNA velocity run",
     )
-    
+
     requiredNamed.add_argument(
         "--metadata",
         required=True,
-        help="Path to the metadata file with sample name, technology, and target cell numbers"
+        help="Path to the metadata file with sample name, technology, and target cell numbers",
     )
 
     requiredNamed.add_argument(
-        "--s3_input_path", required=True, help="The folder with fastq.gz files to align and perform RNA velocity on. Can either have plain fastq files from multiple samples or include sample subfolders."
+        "--s3_input_path",
+        required=True,
+        help="The folder with fastq.gz files to align and perform RNA velocity on. Can either have plain fastq files from multiple samples or include sample subfolders.",
     )
 
     requiredNamed.add_argument(
@@ -76,15 +78,11 @@ def get_parser():
         type=int,
         required=True,
         default=10,
-        help="Number of groups to divide samples "
-        "into for the loompy run",
+        help="Number of groups to divide samples " "into for the loompy run",
     )
-    
+
     requiredNamed.add_argument(
-        "--partition_id",
-        type=int,
-        required=True,
-        help="Index of the sample group",
+        "--partition_id", type=int, required=True, help="Index of the sample group",
     )
 
     # optional arguments
@@ -104,7 +102,7 @@ def main(logger):
     parser = get_parser()
 
     args = parser.parse_args()
-    
+
     root_dir = pathlib.Path(args.root_dir)
 
     if os.environ.get("AWS_BATCH_JOB_ID"):
@@ -123,14 +121,14 @@ def main(logger):
     metadata_dir.mkdir(parents=True)
     metadata_dir = metadata_dir / metadata_name
     s3_metadata_bucket, s3_metadata_prefix = s3u.s3_bucket_and_key(args.metadata)
-    
+
     s3c.download_file(
         Bucket=s3_metadata_bucket,  # just always download this from us-west-2...
         Key=s3_metadata_prefix,
         Filename=str(metadata_dir),
     )
-    
-    technology, sample_name = '', ''
+
+    technology, sample_name = "", ""
     with open(metadata_dir) as fd:
         rd = csv.reader(fd, delimiter="\t", quotechar='"')
         file_content = list()
@@ -138,20 +136,25 @@ def main(logger):
             file_content.append(row)
         file_content = file_content[1:]
         sample_name = file_content[0][0]
-        technology = file_content[0][1]  # need to fix this later to fit tsv file with multiple samples
-    
+        technology = file_content[0][
+            1
+        ]  # need to fix this later to fit tsv file with multiple samples
+
     # check if the input genome is valid
     if args.taxon in reference_genomes_indexes:
         genome_name = reference_genomes_indexes[args.taxon]
     else:
         raise ValueError(f"unknown taxon {args.taxon}")
 
-    if '10x' in technology:
+    if "10x" in technology:
         genome_dir = root_dir / "genome" / "10X" / genome_name
-    elif 'smartseq2' in technology:  # may need to update these after confirming what technology name looks like for smartseq2 data 
-        genome_dir = root_dir / "genome" / "smartseq2" / genome_name  # necessary to separate the reference genome location path for 10x and smartseq2? 
+    elif (
+        "smartseq2" in technology
+    ):  # may need to update these after confirming what technology name looks like for smartseq2 data
+        genome_dir = (
+            root_dir / "genome" / "smartseq2" / genome_name
+        )  # necessary to separate the reference genome location path for 10x and smartseq2?
     genome_dir.mkdir(parents=True)
-    
 
     s3_input_bucket, s3_input_prefix = s3u.s3_bucket_and_key(args.s3_input_path)
 
@@ -167,18 +170,29 @@ def main(logger):
     # download the reference genome index data
     logger.info("Downloading reference genome index files of {}".format(genome_name))
 
-    if '10x' in technology:
+    if "10x" in technology:
         s3_genome_index = f"s3://{S3_REFERENCE['west']}/loompy/10X/{genome_name}"
-    elif 'smartseq2' in technology:
+    elif "smartseq2" in technology:
         s3_genome_index = f"s3://{S3_REFERENCE['west']}/loompy/smartseq2/{genome_name}"
-        
-    s3_genome_index_bucket, s3_genome_index_prefix = s3u.s3_bucket_and_key(s3_genome_index)
-    s3_genome_files_prefix = list(s3u.get_files(s3_genome_index_bucket, s3_genome_index_prefix))
+
+    s3_genome_index_bucket, s3_genome_index_prefix = s3u.s3_bucket_and_key(
+        s3_genome_index
+    )
+    s3_genome_files_prefix = list(
+        s3u.get_files(s3_genome_index_bucket, s3_genome_index_prefix)
+    )
     s3_genome_files_prefix = s3_genome_files_prefix[1:]
-    file_names = list(os.path.basename(file_path) for file_path in s3_genome_files_prefix)
+    file_names = list(
+        os.path.basename(file_path) for file_path in s3_genome_files_prefix
+    )
     genome_name_to_prefix = dict(zip(file_names, s3_genome_files_prefix))
+    
     for file in genome_name_to_prefix.keys():
-        s3c.download_file(Bucket=s3_genome_index_bucket, Key=genome_name_to_prefix[file], Filename=str(genome_dir / file))
+        s3c.download_file(
+            Bucket=s3_genome_index_bucket,
+            Key=genome_name_to_prefix[file],
+            Filename=str(genome_dir / file),
+        )
 
     # extract valid fastq files
     sample_re_smartseq2 = re.compile("([^/]+)_R\d(?:_\d+)?.fastq.gz$")
@@ -201,18 +215,20 @@ def main(logger):
         sample_files = []
         for sample in sample_folder_paths:
             files = [
-            (fn, s)
-            for fn, s in s3u.get_size(s3_input_bucket, s3_input_prefix)
-            if fn.endswith("fastq.gz")]
+                (fn, s)
+                for fn, s in s3u.get_size(s3_input_bucket, s3_input_prefix)
+                if fn.endswith("fastq.gz")
+            ]
             sample_files += files
 
     sample_lists = defaultdict(list)
     sample_sizes = defaultdict(list)
 
     for fn, s in sample_files:
-        if technology == '10x':
+        matched = False
+        if technology == "10x":
             matched = sample_re_10x.search(os.path.basename(fn))
-        elif technology == 'smartseq2':
+        elif technology == "smartseq2":
             matched = sample_re_smartseq2.search(os.path.basename(fn))
         if matched:
             sample_lists[matched.group(1)].append(fn)
@@ -225,7 +241,7 @@ def main(logger):
         result_path = run_dir / "results"
         result_path.mkdir(parents=True)
         os.chdir(result_path)
-        
+
         if sum(sample_sizes[sample]) < args.min_size:
             logger.info(f"{sample} is below min_size, skipping")
             continue
@@ -236,11 +252,11 @@ def main(logger):
             f"{sample_name}.loom",
             sample_name,
             genome_dir,
-            metadata_dir
+            metadata_dir,
         ]
         fastqs = [file for file in sample_lists[sample]]
         command += fastqs
-        
+
         failed = log_command(
             logger,
             command,
