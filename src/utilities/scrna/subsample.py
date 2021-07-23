0cbfe0a486ed728ead55226325c8168e267155a0
atarashansky <alexander.tarashansky@czbiohub.org>
2021-07-23 10:53:43 -0700

    update

--- src/utilities/scrna/subsample.py
+++ src/utilities/scrna/subsample.py
@@ -3,7 +3,7 @@ import numpy as np
 def q(x):
     return np.array(list(x))
 
-def subsample(adata,clname1,clname2=None,nc=10000,MIN=15):
+def subsample(adata,clname1,clname2=None,nc=10000,MIN=15,exclude=[],verbose=False):
     """
     This function goes through each cell type label in `clname1` and then makes sure to select a proportional number
     of each type of cell from `clname2` within that cluster.
@@ -25,11 +25,18 @@ def subsample(adata,clname1,clname2=None,nc=10000,MIN=15):
         Annotation key 2. If None, `clname2` is set equal to `clname1`
         
     nc : int, optional, default 10000
-        Number of cells to target for downsampling. The final number of cells may be slightly lower.
-        Tune `nc` accordingly if you'd like to hit an exact target.
+        Number of cells to target for downsampling. The final number of cells may be different
+        due to the `MIN` parameter.
         
     MIN : int, optional, default 15
         Minimum cluster size after downsampling
+        
+    exclude : list, optional, default []
+        A list of labels to exclude from downsampling. Typically, you'd set
+        exclude=['nan'].
+        
+    verbose : bool, optional, default False
+        If True, outputs subsampling progress.
     
     Returns
     -------
@@ -44,23 +51,25 @@ def subsample(adata,clname1,clname2=None,nc=10000,MIN=15):
     CELLS=[]
     obsn = q(adata.obs_names)
     for i in range(cuu.size):
-        print(i)
-        ix = np.where(cu==cuu[i])[0]
-        a1 = adata[ix,:]
-        lc = q(a1.obs[clname2])
-        lcu,lcuc = np.unique(lc,return_counts=True)
-        z=0
-        cells=[]
-        for j in range(lcu.size):
-            CELLS.append(q(obsn[ix[np.where(lc==lcu[j])[0][np.random.choice(lcuc[j],replace=False,size=int(lcuc[j]*frac))]]]))
-            cells.append(CELLS[-1])
-        z = np.concatenate(cells).size
-        obsnc = obsn[ix]
+        if cuu[i] not in exclude:        
+            if (verbose):
+                print('Subsampling cluster',cuu[i],end='\033[1\ \r')
 
-        if min(MIN,obsnc.size) - z > 0:
-            xx = obsnc[np.in1d(obsnc,np.concatenate(cells),invert=True)]
-            CELLS.append(np.random.choice(xx,replace=False,size = min(xx.size,min(30,obsnc.size) - z)))
-            print(len(CELLS[-1]))
+            ix = np.where(cu==cuu[i])[0]
+            a1 = adata[ix,:]
+            lc = q(a1.obs[clname2])
+            lcu,lcuc = np.unique(lc,return_counts=True)
+            z=0
+            cells=[]
+            for j in range(lcu.size):
+                CELLS.append(q(obsn[ix[np.where(lc==lcu[j])[0][np.random.choice(lcuc[j],replace=False,size=int(lcuc[j]*frac))]]]))
+                cells.append(CELLS[-1])
+            z = np.concatenate(cells).size
+            obsnc = obsn[ix]
+
+            if min(MIN,obsnc.size) - z > 0:
+                xx = obsnc[np.in1d(obsnc,np.concatenate(cells),invert=True)]
+                CELLS.append(np.random.choice(xx,replace=False,size = min(xx.size,min(MIN,obsnc.size) - z)))
     CELLS=np.concatenate(CELLS)
     assert np.unique(CELLS,return_counts=True)[1].max()==1
     CELLS = q(adata.obs_names)[np.in1d(q(adata.obs_names),CELLS)]
