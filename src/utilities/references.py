cf2ff035ccdc02b6a48e659482004f518371d729
aopisco <angela.pisco@czbiohub.org>
2021-12-14 15:09:35 -0800

    Neevor/cellrangerarc (#50)
    * added new cellranger arc ref
    
    * add comma
    
    * add new line

--- src/utilities/references.py
+++ src/utilities/references.py
@@ -23,6 +23,7 @@ reference_genomes = {
     "GRCh38premrna_and_SARSCoV2": "GRCh38premrna_and_SARSCoV2",
     "arc-GRCh38": "refdata-cellranger-arc-GRCh38-2020-A-2.0.0",
     "arc-mm10": "refdata-cellranger-arc-mm10-2020-A-2.0.0",
+    "arc-mm10-mcherry" : "arc-mm10-mcherry",
     "gex-GRCh38": "refdata-gex-GRCh38-2020-A",
     "gex-mm10": "refdata-gex-mm10-2020-A",
     "gex-GRCh38-and-mm10": "refdata-gex-GRCh38-and-mm10-2020-A",
