09e22c5fa4d0bd51ad9420c5a576441d0381008b
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-03 17:59:14 +0100

    wip cellranger count component

--- src/alignment/run_10x_count/script.sh
+++ src/alignment/run_10x_count/script.sh
@@ -2,17 +2,30 @@
 
 # process parameters
 
+SAMPLE_ID=`basename $par_input_path`
+FASTQ_PATH=
+TRANSCRIPTOME_DIR
+SAMPLE_NAME
 # TODO: define other variables
+
+extra_params=( )
+
+if [ -z "$par_cores" ]; then 
+  extra_params+=( "--localcores" "$par_cores" )
+fi
+if [ -z "$par_memory" ]; then 
+  extra_params+=( "--localmem" "$par_memory" )
+fi
 
 cellranger \
   count \
-  --localmem=240 \
   --nosecondary \
   --disable-ui \
   --expect-cells $par_cell_count \
   --id $SAMPLE_ID \
   --fastqs $FASTQ_PATH \
   --transcriptome $TRANSCRIPTIME_DIR \
-  --sample $SAMPLE_NAME
+  --sample $SAMPLE_NAME \
+  "${extra_params[@]}"
 
 # output checks
