6d9ef6fe3b1799da70667d4a861e81d7fef3c608
Samuel Rohan D'Souza <samuelrohandsouza@gmail.com>
2022-02-14 14:03:53 -0800

    Un-tested version of bcl2fastq demux

--- src/demux/bcl2fastq/script.py
+++ src/demux/bcl2fastq/script.py
@@ -2,8 +2,6 @@
 
 # Dependencies:
 # pip: argparse, glob, os, re, subprocess, sys
-# conda: bcl2fastq
-# apt: awscli
 
 import sys
 
@@ -19,15 +17,22 @@ par = {
     "sample_sheet" : "resources_test/bcl/sample_sheet.csv",
     "input": "resources_test/bcl/input/",
     "output": "resources_test/bcl/output/",
-    "output_reports": ""
+    "reports": "resources_test/bcl/reports/",
+    "skip_undetermined": True,
+    "star_structure": True,
 }
-
-meta = { 'resources_dir': '../../helpers' }
 
 ## VIASH END
 
-sys.path.append(meta['resources_dir'])
-from helpers.log_util import log_command
+# Run memory monitor
+command = (
+    "while true;"
+    ' do echo "memory usage" `cat /sys/fs/cgroup/memory/memory.usage_in_bytes`;'
+    ' echo "disk usage" `df -h | grep "/mnt"`;'
+    " sleep 300;"
+    " done"
+)
+p = subprocess.Popen([command], shell=True)
 
 # Run bcl2 fastq
 command = [
@@ -36,61 +41,37 @@ command = [
     "--runfolder-dir", par["input"],
     "--output-dir", par["output"],
 ]
-failed = log_command(
-    logger, command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
-)
-if failed:
+pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+if pipe.communicate()[1]:
     p.kill()
-    raise RuntimeError("bcl2fastq failed, see above for error")
+    raise RuntimeError("bcl2fastq failed: " + str(pipe[1]))
 
-# fix directory structure of the files *before* sync!
-fastqgz_files = glob.glob(os.path.join(output_path, "*fastq.gz"))
-logger.debug("all fastq.gz files\n{}\n\n".format("\n".join(fastqgz_files)))
+# Fix directory structure of the files
+fastqgz_files = glob.glob(os.path.join(par['output'], "*fastq.gz"))
+print("all fastq.gz files\n{}\n\n".format("\n".join(fastqgz_files)))
 
 for fastq_file in fastqgz_files:
     if par["skip_undetermined"] and os.path.basename(fastq_file).startswith(
         "Undetermined"
     ):
-        logger.info("removing {}".format(os.path.basename(fastq_file)))
+        print("removing {}".format(os.path.basename(fastq_file)))
         os.remove(fastq_file)
     elif par["star_structure"]:
         m = re.match("(.+)(_R[12]_001.fastq.gz)", os.path.basename(fastq_file))
         if m:
             sample = m.group(1)
-            if not os.path.exists(os.path.join(output_path, sample)):
-                logger.debug(
-                    "creating {}".format(os.path.join(output_path, sample))
+            if not os.path.exists(os.path.join(par['output'], sample)):
+                print(
+                    "creating {}".format(os.path.join(par['output'], sample))
                 )
-                os.mkdir(os.path.join(output_path, sample))
-            logger.debug("moving {}".format(fastq_file))
+                os.mkdir(os.path.join(par['output'], sample))
+            print("moving {}".format(fastq_file))
             os.rename(
                 fastq_file,
-                os.path.join(output_path, sample, os.path.basename(fastq_file)),
+                os.path.join(par['output'], sample, os.path.basename(fastq_file)),
             )
         else:
-            logger.warning("Warning: regex didn't match {}".format(fastq_file))
+            print("Warning: regex didn't match {}".format(fastq_file))
 
 sys.stdout.flush()
-
-# Move reports data back to S3
-# reports_path = subprocess.check_output(
-#     "ls -d {}".format(
-#         os.path.join(output_path, "Reports", "html", "*", "all", "all", "all")
-#     ),
-#     shell=True,
-# ).rstrip()
-# command = [
-#     "aws",
-#     "s3",
-#     "cp",
-#     "--quiet",
-#     reports_path.decode(),
-#     os.path.join(par["report_path"], par["exp_id"]),
-#     "--recursive",
-# ]
-# for i in range(S3_RETRY):
-#     if not log_command(logger, command, shell=True):
-#         break
-#     logger.info("retrying cp reports")
-# else:
-#     raise RuntimeError("couldn't cp reports")
+print("demux/bcl2fastq run completed.")
--- src/demux/bcl2fastq/test.py
+++ src/demux/bcl2fastq/test.py
@@ -1,17 +1,45 @@
+import subprocess
+## VIASH START
+
+par = {
+    "sample_sheet" : "resources_test/bcl/sample_sheet.csv",
+    "input": "resources_test/bcl/input/",
+    "output": "resources_test/bcl/output/",
+    "reports": "resources_test/bcl/reports/",
+    "skip_undetermined": True,
+    "star_structure": True,
+}
+
+## VIASH END
+
+# Run command: 
 
 # get some data
-input = meta["resources_dir"] + "/path/to/data"
-output = "..."
-executable = "./" + meta["functionality_name"]
+input = 'resources_test/bcls'
+sample_sheet = 'resources_test/sample_sheet/20220125_FS10000331_179_BRB11620-3029.csv'
+output = 'resources_test/output'
+executable = "bcl2fastq"
+
+# Run memory monitor
+command = (
+    "while true;"
+    ' do echo "memory usage" `cat /sys/fs/cgroup/memory/memory.usage_in_bytes`;'
+    ' echo "disk usage" `df -h | grep "/mnt"`;'
+    " sleep 300;"
+    " done"
+)
+p = subprocess.Popen([command], shell=True)
 
 # Run bcl2 fastq
 command = [
-    executable,
-    "--sample-sheet", par["sample_sheet"],
-    "--runfolder-dir", par["input"],
-    "--output-dir", par["output"],
+    "bcl2fastq",
+    "--sample-sheet", sample_sheet,
+    "--runfolder-dir", input,
+    "--output-dir", output,
 ]
-failed = log_command(
-    logger, command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
-)
+pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+if pipe.communicate()[1]:
+    p.kill()
+    raise RuntimeError("bcl2fastq failed: " + str(pipe[1]))
 
+print("Completed Successfully!")
