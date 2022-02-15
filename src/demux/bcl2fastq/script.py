6d179406bc696a7980248ad5075c98b909bc8161
Robrecht Cannoodt <rcannood@gmail.com>
2022-02-15 11:03:06 +0100

    refactor bcl2fastq

--- src/demux/bcl2fastq/script.py
+++ src/demux/bcl2fastq/script.py
@@ -1,77 +1,73 @@
-#!/usr/bin/env python
-
-# Dependencies:
-# pip: argparse, glob, os, re, subprocess, sys
 
 import sys
-
 import glob
 import os
 import re
 import subprocess
-import sys
 
 ## VIASH START
-
 par = {
-    "sample_sheet" : "resources_test/bcl/sample_sheet.csv",
-    "input": "resources_test/bcl/input/",
-    "output": "resources_test/bcl/output/",
-    "reports": "resources_test/bcl/reports/",
+    "sample_sheet" : None,
+    "input": "resources_test/bs_195891710/bcl_data/",
+    "output": "resources_test/bs_195891710/fastqs/",
+    "reports": "resources_test/bs_195891710/reports/",
     "skip_undetermined": True,
     "star_structure": True,
 }
-
 ## VIASH END
 
-# Run memory monitor
-command = (
-    "while true;"
-    ' do echo "memory usage" `cat /sys/fs/cgroup/memory/memory.usage_in_bytes`;'
-    ' echo "disk usage" `df -h | grep "/mnt"`;'
-    " sleep 300;"
-    " done"
-)
-p = subprocess.Popen([command], shell=True)
+if not os.path.isdir(par["output"]):
+    print(f"creating output directory {par['output']}")
+    os.makedirs(par["output"])
 
-# Run bcl2 fastq
+# construct command args
+print("Help")
 command = [
     "bcl2fastq",
-    "--sample-sheet", par["sample_sheet"],
     "--runfolder-dir", par["input"],
     "--output-dir", par["output"],
 ]
-pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-if pipe.communicate()[1]:
-    p.kill()
-    raise RuntimeError("bcl2fastq failed: " + str(pipe[1]))
+if par["sample_sheet"] is not None:
+    command = command + [ "--sample-sheet", par["sample_sheet"] ]
+
+# run bcl2fastq
+with subprocess.Popen(
+    command,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.STDOUT
+) as p:
+    for line in p.stdout:
+        print(line.decode(), end="")
+
+if p.returncode > 0:
+    raise RuntimeError(f"bcl2fastq failed with exit code {p.returncode}")
 
 # Fix directory structure of the files
 fastqgz_files = glob.glob(os.path.join(par['output'], "*fastq.gz"))
-print("all fastq.gz files\n{}\n\n".format("\n".join(fastqgz_files)))
+print("found fastq.gz files:")
+print("\n".join(fastqgz_files))
 
 for fastq_file in fastqgz_files:
-    if par["skip_undetermined"] and os.path.basename(fastq_file).startswith(
-        "Undetermined"
-    ):
-        print("removing {}".format(os.path.basename(fastq_file)))
+    fq_basename = os.path.basename(fastq_file)
+
+    if par["skip_undetermined"] and fq_basename.startswith("Undetermined"):
+        print(f"removing {fq_basename}")
         os.remove(fastq_file)
     elif par["star_structure"]:
-        m = re.match("(.+)(_R[12]_001.fastq.gz)", os.path.basename(fastq_file))
+        m = re.match("(.+)(_R[12]_001.fastq.gz)", fq_basename)
         if m:
             sample = m.group(1)
-            if not os.path.exists(os.path.join(par['output'], sample)):
-                print(
-                    "creating {}".format(os.path.join(par['output'], sample))
-                )
-                os.mkdir(os.path.join(par['output'], sample))
-            print("moving {}".format(fastq_file))
+            sample_dir = os.path.join(par['output'], sample)
+            if not os.path.exists(sample_dir):
+                print(f"creating {sample_dir}")
+                os.mkdir(sample_dir)
+            print(f"moving {fastq_file}")
             os.rename(
                 fastq_file,
-                os.path.join(par['output'], sample, os.path.basename(fastq_file)),
+                os.path.join(sample_dir, fq_basename),
             )
         else:
-            print("Warning: regex didn't match {}".format(fastq_file))
+            print(f"Warning: regex didn't match {fastq_file}")
 
-sys.stdout.flush()
 print("demux/bcl2fastq run completed.")
+sys.stdout.flush()
