2b1428f8f4424cd47d79373d23e7cec433c42c5a
Robrecht Cannoodt <rcannood@gmail.com>
2022-05-23 11:32:25 +0200

    put aws sync in separate script

--- workflows/resources_test_scripts/bs_195891710.sh
+++ workflows/resources_test_scripts/bs_195891710.sh
@@ -2,7 +2,6 @@
 
 DIR=resources_test/bs_195891710
 ID=`basename "$OUT"`
-S3DIR="s3://czbiohub-pipelines/utilities/$DIR"
 
 # download bcl files from basespace
 target/docker/download/download_basespace/download_basespace \
@@ -13,6 +12,3 @@ target/docker/download/download_basespace/download_basespace \
 target/docker/demux/bcl2fastq/bcl2fastq \
   --input "$DIR/bcl/" \
   --output "$DIR/fastqs/"
-
-# upload to s3
-aws s3 sync --profile czb "$DIR" "$S3DIR"
