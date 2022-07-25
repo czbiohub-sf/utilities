57aab68afc30855fa2030319a367abaf8b1c4ce9
aopisco <angela.pisco@czbiohub.org>
2022-07-25 16:13:46 -0700

    adding rhesus_genome as reference options

--- src/utilities/references.py
+++ src/utilities/references.py
@@ -33,6 +33,7 @@ reference_genomes = {
     "mouse_genome_mcherry": "mouse_genome_mcherry",
     "SARS.GRCh38_genome" : "SARS.GRCh38_genome",
     "rhesus_human" : "rhesus_human_genome",
+    "rhesus_genome" : "rhesus_genome"
 }
 
 
